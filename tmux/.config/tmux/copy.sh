7aee80ffc0a055214ce12618ed79391e4dbe82cf
Andrew English <aje299@gmail.com>
2024-03-14 08:50:57 -0700

    Fix tmux copying for multi system

---
+++ tmux/.config/tmux/copy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# https://github.com/tmux-plugins/tmux-yank
+
+command_exists() {
+    local command="$1"
+    type "$command" >/dev/null 2>&1
+}
+
+clipboard_copy_command() {
+	if command_exists "pbcopy"; then
+		# if command_exists "reattach-to-user-namespace"; then
+		# 	echo "reattach-to-user-namespace pbcopy"
+		# else
+		echo "pbcopy"
+		# fi
+	elif command_exists "clip.exe"; then # WSL clipboard command
+		echo "cat | clip.exe"
+	elif command_exists "wl-copy"; then # wl-clipboard: Wayland clipboard utilities
+		echo "wl-copy"
+	elif command_exists "xsel"; then
+		echo "xsel -i --clipboard"
+	elif command_exists "xclip"; then
+		echo "xclip -selection clipboard" 
+	elif command_exists "putclip"; then # cygwin clipboard command
+		echo "putclip"
+	fi
+}
+
+copy_command=$(clipboard_copy_command)
+
+tmux bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "$copy_command"
+tmux bind-key -T copy-mode-vi Y send-keys -X copy-pipe-and-cancel "tmux paste-buffer -p"
+tmux bind-key -T copy-mode-vi M-y send-keys -X copy-pipe-and-cancel "$copy_command; tmux paste-buffer -p"
+# tmux bind-key -T copy-mode-vi ! send-keys -X copy-pipe-and-cancel "$copy_wo_newline_command"
+# if [[ "$(yank_with_mouse)" == "on" ]]; then
+# 	tmux bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X "$(yank_action)" "$copy_command_mouse"
+# fi
+
