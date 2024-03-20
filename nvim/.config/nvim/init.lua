5859d05b3950e8890c0a170fddf0b2f23da042e5
Andrew English <aje299@gmail.com>
2024-03-19 20:59:43 -0700

    Remove surround plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -303,7 +303,6 @@ require("lazy").setup({
 		"echasnovski/mini.nvim",
 		config = function()
 			require("mini.ai").setup({ n_lines = 500 })
-			require("mini.surround").setup()
 			local statusline = require("mini.statusline")
 			statusline.setup({ use_icons = false })
 
