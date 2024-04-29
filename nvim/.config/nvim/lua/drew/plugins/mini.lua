f747d559fee3b4300fa9e12bd75ba05a8117758b
Andrew English <aje299@gmail.com>
2024-04-29 07:57:29 -0700

    Prefer nerd font to no icons in statusline

--- nvim/.config/nvim/lua/drew/plugins/mini.lua
+++ nvim/.config/nvim/lua/drew/plugins/mini.lua
@@ -4,29 +4,29 @@ return {
 		-- require("mini.ai").setup({ n_lines = 500 })
 
 		local statusline = require("mini.statusline")
-		statusline.setup({ use_icons = false })
+		statusline.setup()
 
-		local original_section_git = statusline.section_git
-		statusline.section_git = function(args)
-			local default = original_section_git(args)
-			if default == "" then
-				return ""
-			end
+		-- local original_section_git = statusline.section_git
+		-- statusline.section_git = function(args)
+		-- 	local default = original_section_git(args)
+		-- 	if default == "" then
+		-- 		return ""
+		-- 	end
 
-			local replaced, _ = default:gsub("Git ", "")
-			return replaced
-		end
+		-- 	local replaced, _ = default:gsub("Git ", "")
+		-- 	return replaced
+		-- end
 
-		local original_section_diagnostics = statusline.section_diagnostics
-		statusline.section_diagnostics = function(args)
-			local default = original_section_diagnostics(args)
-			if default == "" then
-				return ""
-			end
+		-- local original_section_diagnostics = statusline.section_diagnostics
+		-- statusline.section_diagnostics = function(args)
+		-- 	local default = original_section_diagnostics(args)
+		-- 	if default == "" then
+		-- 		return ""
+		-- 	end
 
-			local replaced, _ = default:gsub("LSP ", "")
-			return replaced
-		end
+		-- 	local replaced, _ = default:gsub("LSP ", "")
+		-- 	return replaced
+		-- end
 
 		statusline.section_location = function()
 			return "%2l:%-2v"
