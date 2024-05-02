3c59d1db7b86bd3a1c90087009300945c0a53bbb
Andrew <aje299@gmail.com>
2024-05-02 14:43:11 -0700

    Add nvim buffer path copy command

--- nvim/.config/nvim/lua/drew/remap.lua
+++ nvim/.config/nvim/lua/drew/remap.lua
@@ -18,4 +18,10 @@ vim.keymap.set("n", "]q", vim.cmd.cnext, { desc = "Go to next [Q]uickfix Item" }
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set(
+	"n",
+	"<leader>by",
+	vim.cmd.call("setreg('+', expand('%'))"),
+	{ desc = "[B]uffer path [Y]ank to clipboard" }
+)
