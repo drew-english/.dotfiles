9437d7a8a61cb936f8d5abb6ead9301d9bbbba8d
Andrew <aje299@gmail.com>
2024-05-16 10:19:27 -0700

    Change nvim gitsigns -> mini.diff

--- nvim/.config/nvim/lua/drew/plugins/mini.lua
+++ nvim/.config/nvim/lua/drew/plugins/mini.lua
@@ -4,35 +4,66 @@ return {
 		-- require("mini.ai").setup({ n_lines = 500 })
 		require("mini.comment").setup()
 		require("mini.bracketed").setup({
-			undo = { suffix = "" },
+			undo = { suffix = "change" },
 			window = { suffix = "" },
-			yank = { suffix = "" },
+			yank = { suffix = "a change" },
+		})
+
+		require("mini.diff").setup({
+			view = {
+				style = "sign",
+				signs = {
+					add = "+",
+					change = "~",
+					delete = "_",
+				},
+			},
 		})
 
 		local statusline = require("mini.statusline")
 		statusline.setup()
 
-		-- local original_section_git = statusline.section_git
-		-- statusline.section_git = function(args)
-		-- 	local default = original_section_git(args)
-		-- 	if default == "" then
-		-- 		return ""
-		-- 	end
+		local format_summary = function(data)
+			local summary = vim.b[data.buf].minidiff_summary
+			local t = {}
+			if summary.add > 0 then
+				table.insert(t, "+" .. summary.add)
+			end
+			if summary.change > 0 then
+				table.insert(t, "~" .. summary.change)
+			end
+			if summary.delete > 0 then
+				table.insert(t, "-" .. summary.delete)
+			end
+			vim.b[data.buf].minidiff_summary_string = table.concat(t, " ")
+		end
+		local au_opts = { pattern = "MiniDiffUpdated", callback = format_summary }
+		vim.api.nvim_create_autocmd("User", au_opts)
 
-		-- 	local replaced, _ = default:gsub("Git ", "")
-		-- 	return replaced
-		-- end
+		statusline.section_git = function(args)
+			if vim.bo.buftype ~= "" then
+				return ""
+			end
 
-		-- local original_section_diagnostics = statusline.section_diagnostics
-		-- statusline.section_diagnostics = function(args)
-		-- 	local default = original_section_diagnostics(args)
-		-- 	if default == "" then
-		-- 		return ""
-		-- 	end
+			local head = vim.fn.FugitiveHead() or ""
+			local signs = MiniStatusline.is_truncated(args.trunc_width) and "" or (vim.b.minidiff_summary_string or "")
+			local icon = args.icon or ""
 
-		-- 	local replaced, _ = default:gsub("LSP ", "")
-		-- 	return replaced
-		-- end
+			local str = ""
+			if head ~= "" then
+				str = str .. icon .. " " .. head
+			end
+
+			if signs ~= "" then
+				if str ~= "" then
+					str = str .. " "
+				end
+
+				str = str .. signs
+			end
+
+			return str
+		end
 
 		statusline.section_location = function()
 			return "%2l:%-2v"
