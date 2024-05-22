23e9d7f9be1f39e0e26ba5d18fc6c95c2c6aa8dc
Andrew English <aje299@gmail.com>
2024-05-21 17:53:10 -0700

    Add custom file open commands for mini.files

--- nvim/.config/nvim/lua/drew/custom/mini.lua
+++ nvim/.config/nvim/lua/drew/custom/mini.lua
@@ -64,3 +64,26 @@ end
 statusline.section_location = function()
 	return "%2l:%-2v"
 end
+
+-- Add custom keymaps to mini.files
+local files_open_new_win = function(win_cmd)
+	return function()
+		local new_target_window
+		vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+			vim.cmd(win_cmd)
+			new_target_window = vim.api.nvim_get_current_win()
+		end)
+
+		MiniFiles.set_target_window(new_target_window)
+		MiniFiles.go_in({ close_on_file = true })
+	end
+end
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferCreate",
+	callback = function(args)
+		local buf_id = args.data.buf_id
+		vim.keymap.set("n", "<c-v>", files_open_new_win("vs"), { buffer = buf_id, desc = "Open in vertical split" })
+		vim.keymap.set("n", "<c-t>", files_open_new_win("tabnew"), { buffer = buf_id, desc = "Open in new tab" })
+	end,
+})
