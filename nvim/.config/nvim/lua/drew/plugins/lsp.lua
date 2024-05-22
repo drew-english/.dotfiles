81282a6eb5f77d52255ef9d24df2be9bb6111b32
Andrew English <aje299@gmail.com>
2024-05-22 08:52:19 -0700

    Update lsp config

--- nvim/.config/nvim/lua/drew/plugins/lsp.lua
+++ nvim/.config/nvim/lua/drew/plugins/lsp.lua
@@ -50,38 +50,27 @@ return {
 		--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-		local servers = {
-			gopls = {},
-			lua_ls = {
+		local server_cfgs = {
+			zls = {
 				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						workspace = {
-							checkThirdParty = false,
-							library = {
-								"${3rd}/luv/library",
-								unpack(vim.api.nvim_get_runtime_file("", true)),
-							},
-						},
-						completion = {
-							callSnippet = "Replace",
-						},
-					},
+					enable_build_on_save = true,
 				},
 			},
 		}
 
-		local ensure_installed = vim.tbl_keys(servers or {})
-		vim.list_extend(ensure_installed, {
-			"stylua", -- Used to format lua code
-		})
+		local ensure_installed = {
+			"gopls",
+			"lua_ls",
+			"stylua",
+			"solargraph",
+		}
 
 		require("mason").setup()
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 		require("mason-lspconfig").setup({
 			handlers = {
 				function(server_name)
-					local server = servers[server_name] or {}
+					local server = server_cfgs[server_name] or {}
 
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 					require("lspconfig")[server_name].setup(server)
