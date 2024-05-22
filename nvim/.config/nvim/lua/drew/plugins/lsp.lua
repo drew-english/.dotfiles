1a4ab483bb5e79103878d6f8f5028c3777aff0ce
Andrew <aje299@gmail.com>
2024-05-22 14:00:52 -0700

    Fix lsp

--- nvim/.config/nvim/lua/drew/plugins/lsp.lua
+++ nvim/.config/nvim/lua/drew/plugins/lsp.lua
@@ -1,26 +1,27 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
+		"folke/neodev.nvim",
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		{ "j-hui/fidget.nvim", opts = {} },
 	},
 	config = function()
+		require("neodev").setup()
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
 				local map = function(keys, func, desc)
 					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
-				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+				vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+				map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+				map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 				map("<leader>csd", require("telescope.builtin").lsp_document_symbols, "[C]ode [S]ymbols [D]ocument")
-				map(
-					"<leader>csw",
-					require("telescope.builtin").lsp_dynamic_workspace_symbols,
-					"[C]ode [S]ymbols [W]orkspace"
-				)
 				map("<leader>cr", vim.lsp.buf.rename, "[C]ode [R]ename")
 				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 				map("<leader>f", vim.lsp.buf.format, "Format file")
