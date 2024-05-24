66571c4ba64cda2fb0268e1e3a5412af43c3b225
Andrew <aje299@gmail.com>
2024-05-24 09:06:24 -0700

    Add relnum and make splits below + right

--- nvim/.config/nvim/lua/drew/set.lua
+++ nvim/.config/nvim/lua/drew/set.lua
@@ -1,37 +1,42 @@
+local opt = vim.opt
+
 -- Make line numbers default
-vim.opt.number = true
--- vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
 -- Enable mouse mode
-vim.opt.mouse = "a"
+opt.mouse = "a"
 
 -- Don't show the mode, since it's already in status line
-vim.opt.showmode = false
+opt.showmode = false
 
-vim.opt.breakindent = true
-vim.opt.undofile = true
+opt.breakindent = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
-vim.opt.signcolumn = "yes"
-vim.opt.smartindent = true
+opt.signcolumn = "yes"
+opt.smartindent = true
 
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
+opt.updatetime = 250
+opt.timeoutlen = 300
 
 -- Preview substitutions live
-vim.opt.inccommand = "split"
+opt.inccommand = "split"
 
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.hlsearch = true
+opt.cursorline = true
+opt.scrolloff = 10
+opt.hlsearch = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.splitbelow = true
+opt.splitright = true
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
