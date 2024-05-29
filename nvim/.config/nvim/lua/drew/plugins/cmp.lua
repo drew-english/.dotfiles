2e07a6288e928f3bf3b93b82023279ff29135637
Andrew English <aje299@gmail.com>
2024-05-28 21:19:14 -0700

    Add debug plugins for ruby

--- nvim/.config/nvim/lua/drew/plugins/cmp.lua
+++ nvim/.config/nvim/lua/drew/plugins/cmp.lua
@@ -1,8 +1,7 @@
-return { -- Autocompletion
+return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
 		{
 			"L3MON4D3/LuaSnip",
 			build = "make install_jsregexp",
