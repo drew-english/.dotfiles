9e439c64c633ca085906f671739231f8e8295a29
Andrew <aje299@gmail.com>
2024-06-03 14:30:10 -0700

    Add endwise

--- nvim/.config/nvim/lua/drew/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/drew/plugins/treesitter.lua
@@ -1,11 +1,15 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"RRethy/nvim-treesitter-endwise",
+	},
 	build = ":TSUpdate",
 	opts = {
 		ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
 		auto_install = true,
 		highlight = { enable = true },
 		indent = { enable = true },
+		endwise = { enable = true },
 	},
 	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
