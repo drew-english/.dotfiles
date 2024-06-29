347f870d661c4716ec8cf979ed9cf2d8e8f5874c
Andrew English <aje299@gmail.com>
2024-06-29 10:27:31 -0700

    Add copy buffer file path keybind

--- nvim/.config/nvim/lua/drew/remap.lua
+++ nvim/.config/nvim/lua/drew/remap.lua
@@ -1,18 +1,22 @@
+local set = vim.keymap.set
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("n", "<leader>q", vim.cmd.copen, { desc = "Open [Q]uickfix list" })
-vim.keymap.set("n", "<leader>Q", vim.cmd.cclose, { desc = "Close [Q]uickfix list" })
+set("n", "<leader>q", vim.cmd.copen, { desc = "Open [Q]uickfix list" })
+set("n", "<leader>Q", vim.cmd.cclose, { desc = "Close [Q]uickfix list" })
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+set("x", "<leader>p", [["_dP]])
+set({ "n", "v" }, "<leader>y", [["+y]])
+set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
-vim.keymap.set("n", "<leader>X", "<cmd>source %<CR>", { desc = "Execute the current file" })
+set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
+set("n", "<leader>X", "<cmd>source %<CR>", { desc = "Execute the current file" })
+
+set("n", "<leader>%", "<cmd>call setreg('+', expand('%'))<CR>", { desc = "Copy buffer file path" })
