641e28eecce018eb07a4cc98fafea1a9974ac0af
Andrew <aje299@gmail.com>
2024-10-21 10:05:43 -0700

    Update colorscheme for ruby

--- nvim/.config/nvim/lua/drew/plugins/colorshceme.lua
+++ nvim/.config/nvim/lua/drew/plugins/colorshceme.lua
@@ -1,36 +1,26 @@
 return {
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	priority = 1000,
-	-- 	init = function()
-	-- 		vim.cmd.colorscheme("tokyonight-moon")
-	-- 		vim.cmd.hi("Comment gui=none")
-	-- 	end,
-	-- },
-	-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{
 		"EdenEast/nightfox.nvim",
-        priority = 1000,
+		priority = 1000,
 		init = function()
 			require("nightfox").setup({
 				groups = {
 					nightfox = {
 						LineNr = { fg = "#737272" },
+						Special = { fg = "#95afb7" },
 					},
 				},
-                palettes = {
+				palettes = {
 					nightfox = {
-                        bg0 = "#171b20",
-                        bg1 = "#1f242a",
-                        bg3 = "#323a46",
-                        sel0 = "#353d47",
-                        sel1 = "#495565",
+						bg0 = "#171b20",
+						bg1 = "#1f242a",
+						bg3 = "#323a46",
+						sel0 = "#353d47",
+						sel1 = "#495565",
 					},
-                },
+				},
 			})
 			vim.cmd.colorscheme("nightfox")
 		end,
 	},
-	-- { "rose-pine/neovim", name = "rose-pine" },
-	-- { "shaunsingh/nord.nvim" },
 }
