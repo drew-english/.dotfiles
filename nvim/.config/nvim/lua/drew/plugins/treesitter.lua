faa061c45da3f58f736b489de4ac35397d147704
Andrew English <aje299@gmail.com>
2025-05-12 22:28:45 -0700

    Add treesitter-context

--- nvim/.config/nvim/lua/drew/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/drew/plugins/treesitter.lua
@@ -1,20 +1,26 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"RRethy/nvim-treesitter-endwise",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"RRethy/nvim-treesitter-endwise",
+		},
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "ruby" },
+			},
+			indent = { enable = true, disable = { "ruby" } },
+			endwise = { enable = true },
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
 	},
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
-		auto_install = true,
-		highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { 'ruby' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-		endwise = { enable = true },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		after = "nvim-treesitter",
 	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
 }
