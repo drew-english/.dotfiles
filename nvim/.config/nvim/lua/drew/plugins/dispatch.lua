eab32a73be487349415c42d05c47154817ea076a
Andrew <aje299@gmail.com>
2025-08-08 13:22:43 -0700

    Fix dispatching file after line

--- nvim/.config/nvim/lua/drew/plugins/dispatch.lua
+++ nvim/.config/nvim/lua/drew/plugins/dispatch.lua
@@ -7,9 +7,20 @@ return {
 			vim.opt.shell = "/bin/zsh"
 		end
 
+        local strip_line = function()
+            if string.find(vim.b.dispatch, ":") then
+                vim.b.dispatch = string.gsub(vim.b.dispatch, ":%d+", "")
+            end
+        end
+
 		local run_dispatch = function()
 			vim.g.last_dispatch = vim.b.dispatch
 			vim.cmd("Dispatch")
+		end
+
+		local run_dispatch_file = function()
+            strip_line()
+            run_dispatch()
 		end
 
 		local run_last_dispatch = function()
@@ -26,16 +37,13 @@ return {
                 return
             end
 
-            if string.find(vim.b.dispatch, ":") then
-                vim.b.dispatch = string.gsub(vim.b.dispatch, ":%d+", "")
-            end
-
+            strip_line()
             vim.b.dispatch = vim.b.dispatch .. ":" .. vim.fn.getcurpos()[2]
             run_dispatch()
         end
 
 		vim.keymap.set("n", "<leader>dx", "<cmd>AbortDispatch<CR>", { desc = "[D]ispatch Abort" })
-		vim.keymap.set("n", "<leader>df", run_dispatch, { desc = "[D]ispatch [F]ile" })
+		vim.keymap.set("n", "<leader>df", run_dispatch_file, { desc = "[D]ispatch [F]ile" })
 		vim.keymap.set("n", "<leader>dr", run_last_dispatch, { desc = "[D]ispatch [R]erun" })
 		vim.keymap.set("n", "<leader>dl", run_dispatch_line, { desc = "[D]ispatch [L]ine" })
 	end,
