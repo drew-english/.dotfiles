2e848915bc9fc195bd0963560c3c656b219c3397
Andrew English <andrew.english@hp.com>
2026-02-06 15:41:35 -0800

    feat: Create command to convert current buffer text from markdown to jira md

--- nvim/.config/nvim/lua/drew/init.lua
+++ nvim/.config/nvim/lua/drew/init.lua
@@ -11,6 +11,21 @@ vim.api.nvim_create_user_command("DiagnosticsToggleVirtualText", function()
 	end
 end, {})
 
+vim.api.nvim_create_user_command("MarkdownToJira", function()
+    -- current buffer content
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    local content = table.concat(lines, "\n")
+
+    local result = vim.fn.system("pandoc -f markdown -t jira", content)
+    if vim.v.shell_error ~= 0 then
+        print("Error converting markdown to jira format: " .. result)
+        return
+    end
+
+    vim.fn.setreg("+", result)
+    print("Content copied to clipboard")
+end, {})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("drew-highlight-yank", { clear = true }),
